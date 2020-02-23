af79ceec7cef398788a9686c2756a81b7fce5391
Luis Gonçalves <exnlesi@gmail.com>
2020-02-23 18:08:02 +0000

    added fetch sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+const Layout = styled.div`
+  h1 {
+    color: red;
+    font-size: 50px;
+  }
+`;
 
-export default () => <Title>My page</Title>
+const Index = (props: any) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map((show: any) => (
+        <li key={show.id}>
+          <Link href={`/events/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map((entry: any) => entry.show)
+  };
+};
+
+export default Index;
